5c24933f9ae893c0edd99833eff6ec563e4fbdb0
pedrog1997 <pedro_0797@hotmail.com>
2018-06-08 15:23:00 +0200

    First Commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChessEngine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+	    Console.Read();
+        }
+    }
+}
